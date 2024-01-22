3a6db662c15e201dafe219106676b0e1c939e025
leocareer <leo.ph.career@gmail.com>
2024-01-22 04:23:21 +0100

    search

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,14 +1,10 @@
-//const cypress = require("cypress");
-//const { describe } = require("mocha");
-
 describe('search', () => {
   it('search', () => {
     cy.viewport(1025,360)
-    cy.visit("http://b12veganshop.com")
+    cy.visit('http://b12veganshop.com')
     cy.get('.woodmart-search-form').should('be.visible')
-    //cy.get(".searchform").type()
-   // cy.get(".searchsubmit")
-   // cy.get("searchsubmit").click()
-   // cy.get(".lighter").contains("поиска")
+    cy.get('.search-style-default').type('пельмени')
+    cy.get('.search-style-default .searchsubmit').click()
+    cy.get('body').contains('пельмени')
   })
 })
